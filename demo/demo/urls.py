103c4350de1fe7d9aa427a819d6f7cf8986155ea
sanju4725 <sanju4725@gmail.com>
2025-03-18 23:55:36 +0530

    Work Baby Work

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ecommercesite project.
+URL configuration for demo project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include("myapp.urls"))
+]
