a9a16f58ee2fc097c637ba2d7dc9caa147f47815
sanju4725 <sanju4725@gmail.com>
2025-05-29 18:21:46 +0530

    Added FrontEnd Files

--- ecommerce_site/ecommerce_site/settings.py
+++ ecommerce_site/ecommerce_site/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'ecommerce_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
